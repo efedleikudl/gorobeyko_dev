4e7b7ddd3471c2cc9def6f6466faa8e5006cbc99
efedleikudl <efedleikudl@code.com>
2026-07-23 11:34:44 +0000

    build: support proxied webpack development

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,23 @@
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js'
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const baseConfig = {
   output: 'export',
   trailingSlash: true,
+}
+
+const nextConfig = (phase) => {
+  if (phase !== PHASE_DEVELOPMENT_SERVER) return baseConfig
+
+  return {
+    ...baseConfig,
+    allowedDevOrigins: [
+      '127.0.0.1',
+      'code.gorobeyko.com',
+      '*.vscode-cdn.net',
+    ],
+    assetPrefix: '/proxy/3000',
+  }
 }
 
 export default nextConfig
